019971d9d22d6b31f0449be545827f6f439c4aad
mercea violeta <consilierjuridic.violeta@gmail.com>
2021-03-11 21:45:49 +0200

    recover list from json

--- functions.js
+++ functions.js
@@ -1,198 +1,25 @@
-const API = {
-    CREATE: {
-        URL: "http://localhost:3000/teams/create",
-        METHOD: "POST"
-    },
-    READ: {
-        URL: "http://localhost:3000/teams",
-        METHOD: "GET"
-    },
-    UPDATE: {
-        URL: "http://localhost:3000/teams/update",
-        METHOD: "PUT"
-    },
-    DELETE: {
-        URL: "http://localhost:3000/teams/delete",
-        METHOD: "DELETE"
-    }
-};
+let allTeams = [];
 
-// for demo purposes...
-if (true || location.host === "nmatei.github.io") {
-    API.READ.URL = "data/teams.json";
-    API.DELETE.URL = "data/delete.json";
-    API.CREATE.URL = "data/create.json";
-    API.UPDATE.URL = "data/update.json";
-
-    API.READ.METHOD = "GET";
-    API.DELETE.METHOD = "GET";
-    API.CREATE.METHOD = "GET";
-    API.UPDATE.METHOD = "GET";
-}
-
-let editId;
-
-function insertPersons(persons) {
-    const tbody = document.querySelector('#list tbody');
-    tbody.innerHTML = getPersonsHtml(persons);
-}
-
-function getPersonsHtml (persons) {
-    return persons.map(getPersonHtml).join("");
-}
-
-function getPersonHtml (person) {
-    const projectUrl = person.projectUrl;
-    return `<tr>
-        <td>${person.group}</td>    
-        <td>${person.members.split(/\s*,\s*/).join("<br>")}</td>
-        <td>${person.projectName}</td>
-        <td><a target="_blank" href="${projectUrl}">Github</a></td>
-        <td>
-            <a href="#" class="delete-row" data-id="${person.id}">&#10006;</a>
-            <a href="#" class="edit-row" data-id="${person.id}">&#9998;</a>
-        </td>
-    </tr>`;
-}
-
-let allPersons = [];
-
-function loadList() {
-    fetch(API.READ.URL)
-        .then(res => res.json())
-        .then(data => {
-            allPersons = data;
-            insertPersons(data);
-        });
-}
-
-loadList();
-
-function searchPersons(text) {
-    text = text.toLowerCase();
-    return allPersons.filter(person => {
-        return person.members.toLowerCase().indexOf(text) > -1 ||
-            person.projectName.toLowerCase().indexOf(text) > -1;
-    });
-}
-
-function saveTeamMember() {
-    const members = document.querySelector("#list input[name=members]").value;
-    const projectName = document.querySelector("input[name=projectName]").value;
-    const projectUrl = document.querySelector("input[name=projectUrl]").value;
+function getHtmlTeams(teams){
+    return teams.map(team => {
+        return `<tr>
+        <td>${team.members}</td>
+        <td>${team.name}</td>
+        <td>${team.url}</td>
+    </tr>`
+    }).join("")
     
-    const person = {
-        members,
-        projectName,
-        projectUrl
-    };
-    console.info('saving...', person, JSON.stringify(person));
-
-    const method = API.CREATE.METHOD;
-    fetch(API.CREATE.URL, {
-        method,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: method === "GET" ? null : JSON.stringify(person)
-    })
-        .then(res => res.json())
-        .then(r => {
-            console.warn(r);
-            if (r.success) {
-                loadList();
-            }
-        });
 }
+function showTeams(teams) {
+    const html = getHtmlTeams(teams);
+     
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = html;
 
-function updateTeamMember() {
-    const members = document.querySelector("#list input[name=members]").value;
-    const projectName = document.querySelector("input[name=projectName]").value;
-    const projectUrl = document.querySelector("input[name=projectUrl]").value;
-    
-    const person = {
-        id: editId,
-        members,
-        projectName,
-        projectUrl
-    };
-    console.info('updating...', person, JSON.stringify(person));
-
-    const method = API.UPDATE.METHOD;
-    fetch(API.UPDATE.URL, {
-        method,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: method === "GET" ? null : JSON.stringify(person)
-    })
-        .then(res => res.json())
-        .then(r => {
-            if (r.success) {
-                loadList();
-            }
-        });
 }
-
-function deleteTeamMember(id) {
-    const method = API.UPDATE.METHOD;
-    fetch(API.DELETE.URL, {
-        method,
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: method === 'GET' ? null : JSON.stringify({ id })
-    })
-        .then(r => r.json())
-        .then(r => {
-            if (r.success) {
-                loadList();
-            }
-        });
-}
-
-function populateCurrentMember(id) {
-    var person = allPersons.find(person => person.id === id);
-
-    editId = id;
-    
-    const members = document.querySelector("#list input[name=members]");
-    const projectName = document.querySelector("input[name=projectName]");
-    const projectUrl = document.querySelector("input[name=projectUrl]");
-
-    members.value = person.members;
-    projectName.value = person.projectName;
-    projectUrl.value = person.projectUrl;
-}
-
-function addEventListeners() {
-    const search = document.getElementById('search');
-    search.addEventListener("input", e => {
-        const text = e.target.value;
-        const filtrate = searchPersons(text);
-        insertPersons(filtrate);
+fetch("teams.json")
+    .then(r => r.json())
+    .then(teams =>{
+        allTeams = teams;
+        showTeams(teams);
     });
-    
-    const saveBtn = document.querySelector("#list tfoot button");
-    saveBtn.addEventListener("click", () => {
-        if (editId) {
-            updateTeamMember();
-        } else {
-            saveTeamMember();
-        }
-    });
-
-    const table = document.querySelector("#list tbody");
-    table.addEventListener("click", (e) => {
-        const target = e.target;
-        if (target.matches("a.delete-row")) {
-            const id = target.getAttribute("data-id");
-            deleteTeamMember(id);
-        } else if (target.matches("a.edit-row")) {
-            const id = target.getAttribute("data-id");
-            populateCurrentMember(id);
-        }
-    });
-}
-
-addEventListeners();
