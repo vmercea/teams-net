1524718c5c3731a8d07d8a1fdc62bad6f62f19b0
mercea violeta <consilierjuridic.violeta@gmail.com>
2021-03-12 17:21:00 +0200

    button Save

--- functions.js
+++ functions.js
@@ -23,3 +23,30 @@ fetch("teams.json")
         allTeams = teams;
         showTeams(teams);
     });
+
+function addTeam(team){
+    fetch("ädd.json",  {
+        metod: "POST",
+        body:JSON.stringify(team)
+    })
+
+    .them(r=>r.json())
+    .them(status=> {
+        console.warn("status", status);
+
+    });
+}
+function saveTeam{
+    const members = document.querySelector("input[name=members]").value;
+    const name = document.querySelector("input[name=name]").value;
+    const url = document.querySelector("input[name=url]").value;
+
+        const team = {
+            name:name,
+            members:members,
+            url:url
+        }
+
+        addTeam(team);
+
+}
